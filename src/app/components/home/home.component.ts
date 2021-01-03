28c1bc07586705a64e84b2882a1d06c3d1f83e3b
madhuvayya <madhu.vayya@gmail.com>
2021-01-04 01:05:11 +0530

    Added code to display employee data from the employeePayrollData object array

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -6,10 +6,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-
+    empPayrollListLocal: Array<any> = [
+        {
+            _name: 'Narayan Mahadevan',
+            _gender: 'male',
+            _department: [
+                'Engineering',
+                'Finance'
+            ],
+            _salary: '50000',
+            _startDate: '29 oct 2019',
+            _note: '',
+            _id: new Date().getDate,
+            _profilePic: '../assets/profile-images/Ellipse -2.png'  
+        },
+        {
+            _name: 'Anarpa Shashank Keerthi Kumar',
+            _gender: 'female',
+            _department: [
+                'Sales'
+            ],
+            _salary: '40000',
+            _startDate: '29 oct 2019',
+            _note: '',
+            _id: new Date().getTime()+1,
+            _profilePic: '../assets/profile-images/Ellipse -1.png'  
+        }
+      ];        
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  
 }
