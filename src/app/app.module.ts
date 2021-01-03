e4fe813d4b666ab694a4e3bb52663971d5c1992e
madhuvayya <madhu.vayya@gmail.com>
2021-01-03 14:27:29 +0530

    Refactored code to dynamically display the profiles and departmets

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component'
@@ -12,7 +14,9 @@ import { PayrollFormComponent } from './components/payroll-form/payroll-form.com
     PayrollFormComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/payroll-form/payroll-form.component.ts
+++ src/app/components/payroll-form/payroll-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, FormBuilder, Validators, FormArray } from '@angular/forms';
 
 @Component({
   selector: 'payroll-form',
@@ -6,10 +7,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./payroll-form.component.scss']
 })
 export class PayrollFormComponent implements OnInit {
+  profileArray: any[];
+  employeeForm: any;
+  allDepartments: Array<string> = ["Hr","Sales","Finance", "Engineer", "Others"];               
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+    this.employeeForm = this.formBuilder.group({
+      name: ['',Validators.required],
+      gender: ['',Validators.required],
+      department: this.addDepartmentControls(),
+      salary: ['',Validators.required],
+      day: ['1',Validators.required],
+      month: ['Jan',Validators.required],
+      year: ['2020',Validators.required],
+      notes: [],
+      profilePic:['',Validators.required]
+    });
+    this.profileArray=[   {path:"../assets/profile-images/Ellipse -3.png"},
+                          {path:'../assets/profile-images/Ellipse -1.png'},
+                          {path:'../assets/profile-images/Ellipse -8.png'},
+                          {path:'../assets/profile-images/Ellipse -7.png' }
+                      ];
   }
 
+  addDepartmentControls() {
+    const arr = this.allDepartments.map(department =>{
+      return this.formBuilder.control(false);
+    })
+    return this.formBuilder.array(arr);
+  }
+
+  get allDepartmentsArray() {
+    return <FormArray>this.employeeForm.get('department');
+  }
 }
