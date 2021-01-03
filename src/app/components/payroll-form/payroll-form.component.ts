5444e3090459a1f31d03a68045a251cb229e96bb
madhuvayya <madhu.vayya@gmail.com>
2021-01-03 18:38:45 +0530

    Added code to validate the user name

--- src/app/components/payroll-form/payroll-form.component.ts
+++ src/app/components/payroll-form/payroll-form.component.ts
@@ -10,13 +10,14 @@ export class PayrollFormComponent implements OnInit {
   profileArray: any[];
   employeeForm: any;
   allDepartments: Array<string> = ["Hr","Sales","Finance", "Engineer", "Others"];
-  selectedSalary: number = 400000;               
+  selectedSalary: number = 400000;
+  errorText: string;               
 
   constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
     this.employeeForm = this.formBuilder.group({
-      name: ['',Validators.required],
+      name: ['',[Validators.required, Validators.pattern('^[A-Z]{1}[a-zA-Z\\s]{2,}$')]],
       gender: ['',Validators.required],
       department: this.addDepartmentControls(),
       salary: ['',Validators.required],
@@ -40,11 +41,20 @@ export class PayrollFormComponent implements OnInit {
     return this.formBuilder.array(arr);
   }
 
+  get name() {
+    return this.employeeForm.get('name');
+  }
+
   get allDepartmentsArray() {
     return <FormArray>this.employeeForm.get('department');
   }
 
   onSalaryChange() {
     this.selectedSalary = this.employeeForm.value.salary;
+  }
+
+  save(){
+    console.log(this.employeeForm.value);
+    alert(this.employeeForm.value.name);
   }
 }
