55d9d027a49703086f744dc8ff3e5ca009b45c61
madhuvayya <madhu.vayya@gmail.com>
2021-01-04 12:30:05 +0530

    Added code to delete the employee details from employee data stored in local storage

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -9,16 +9,28 @@ import { Router } from '@angular/router';
 export class HomeComponent implements OnInit {
     empCount: number;
     empPayrollList: Array<any>;
-
-  constructor(private router: Router) { }
-
-  ngOnInit() {
-    this.empPayrollList = localStorage.getItem('EmployeePayrollList') ? 
-                                    JSON.parse(localStorage.getItem('EmployeePayrollList')) : [];
-    this.processEmployeePayrollDataResponse();
-  }
     
-  processEmployeePayrollDataResponse() {
-    this.empCount = this.empPayrollList.length;  
-  }
+    constructor(private router: Router) { }
+    
+    ngOnInit() {
+        this.empPayrollList = localStorage.getItem('EmployeePayrollList') ? 
+                                    JSON.parse(localStorage.getItem('EmployeePayrollList')) : [];
+        this.processEmployeePayrollDataResponse();
+    }
+    
+    processEmployeePayrollDataResponse() {
+        this.empCount = this.empPayrollList.length;  
+    }
+
+    remove ($event) {
+        let id = $event.target.id;            
+        let empPayrollData = this.empPayrollList.find(empData => empData.id == id);
+        if (!empPayrollData) return;
+        const index = this.empPayrollList
+                        .map(empData => empData.id)
+                        .indexOf(empPayrollData.id);
+        this.empPayrollList.splice(index, 1);
+        localStorage.setItem("EmployeePayrollList",JSON.stringify(this.empPayrollList));
+        this.processEmployeePayrollDataResponse();              
+    }
 }
